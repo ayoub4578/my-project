c4e3bb829bd020a6f4cc3a8163ab8690cf4bcb4c
ayoub4578 <elarqam.ayoube@gmail.com>
2025-08-22 18:26:11 +0100

    pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Signin from './pages/signin';   // ensure the filename matches exactly
+import Profile from './pages/Profile';
+import About from './pages/about';
+import SignUp from './pages/SignUp';
 
 export default function App() {
-  return  <h1 className='text-red-400'>  app </h1> ;
-      
-
-  
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </Router>
+  );
 }
