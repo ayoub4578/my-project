d6fc1ae9de4d7beb19921b2be8dec73a015dcb92
ayoub4578 <elarqam.ayoube@gmail.com>
2025-09-21 05:37:17 +0100

    creat sign api routes

--- api/controllers/auth_controllers.js
+++ api/controllers/auth_controllers.js
@@ -1,5 +1,7 @@
 import User from '../models/user.model.js';
-import bcrypt from 'bcryptjs';
+import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { errorHandler } from '../utlis/error.js';
 
 export const signup =  async (req, res,next) => {
 
@@ -16,3 +18,20 @@ export const signup =  async (req, res,next) => {
 
 
 };
+export const signin = async (req, res,next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email }); 
+        if (!validUser) return next(errorHandler (404, 'User not found'));
+        const validpassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validpassword) return next(errorHandler (400, 'wrong credentials'));
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
+        const { password:pass,...rest} = validUser._doc;
+        res.cookie('acces_token', token, { httpOnly: true})
+        .status(200)
+        .json({ rest });
+        }
+        catch (error) {
+            next(error);
+        }
+}
